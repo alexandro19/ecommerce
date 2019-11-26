ff2e10be09aade37a79fde57c642e499f3708400
Alexandro <ale_go121@hotmail.com>
2019-11-26 21:10:16 -0200

    Criamos a classe tpl

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use \Hcode\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("select * from tb_users");
 
-	echo json_encode($results);
+$page = new Page(); // Nesse momento ele chama o metodo construct. E esse metodo construct carrega a pagina header.html. Verifique seu código.
+
+$page->setTpl("index"); // Carrega o conteudo que está na tela de index.html
+	
 });
 
-$app->run();
+$app->run(); // faz a ignição ou seja roda tudo isso que definimos a cima.
 
  ?>
---
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -0,0 +1,50 @@
+<?php 
+	
+	namespace Hcode;
+
+	use Rain\Tpl;
+
+	class Page{
+
+		private $tpl;
+		private $options = [];
+		private $defaults = [
+			"data"=>[]
+		];
+
+		public function __construct($opts = array()){
+
+			$this->options = array_merge($this->defaults, $opts);
+
+			$config = array(
+				"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+				"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
+				"debug"         => false // set to false to improve the speed
+		   	);
+
+			Tpl::configure( $config );
+			$this->tpl = new tpl;
+
+			$this->setData($this->options["data"]);
+
+			$this->tpl->draw("header");
+		}
+
+		private function setData($data = array()){ // Esse metodo é responsável por passar os dados para o templete
+			foreach ($data as $key => $value) {
+				$this->tpl->assign($key, $value);
+			}			
+		}
+
+		public function setTpl($nome, $data  = array(), $returnHTML = false){
+			$this->setData($data);
+			$this->tpl->draw($nome, $returnHTML);
+		} 
+
+		public function __destruct(){
+			$this->tpl->draw("footer");
+			
+		}
+	}
+
+ ?>
